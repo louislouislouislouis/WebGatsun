48014fc797f7b73c55816e9871fe6a8853efd727
louislouislouislouis <louis.raphael.lombard@gmail.com>
2021-02-05 09:03:50 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import MainNav from "./Components/Nav/MainNav";
+import Waitings from "./Components/Shared/Waitings";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import User from "./Pages/User";
+import Myflow from "./Pages/Myflow";
 
 function App() {
+  const [isLoggedin, setisLoggedin] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <MainNav />
+      {isLoggedin && <Waitings />}
+      <Switch>
+        <Route path="/myprofile" exact={true}>
+          <User />
+        </Route>
+        <Route path="/:userId" exact={true}>
+          <Myflow />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
