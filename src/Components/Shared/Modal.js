b33347dcd189f55f28a458de4187ba04f51cb526
louislouislouislouis <louis.raphael.lombard@gmail.com>
2021-03-03 00:05:48 +0100

    AllUser Page Finish

--- src/Components/Shared/Modal.js
+++ src/Components/Shared/Modal.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
 
@@ -7,25 +7,23 @@ import "./modal.css";
 
 const ModamOverlay = (props) => {
   const content = (
-    <React.Fragment>
-      <div className={`modal ${props.className}`} style={props.style}>
-        <header className={`modal__header ${props.headerClass}`}>
-          <h2>{props.header}</h2>
-        </header>
-        <form
-          onSubmit={
-            props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
-          }
-        >
-          <div className={`modal__content ${props.contentClass}`}>
-            {props.children}
-          </div>
-          <footer className={`modal__footer ${props.footerClass}`}>
-            {props.footer}
-          </footer>
-        </form>
-      </div>
-    </React.Fragment>
+    <div className={`modal ${props.className}`} style={props.style}>
+      <header className={`modal__header ${props.headerClass}`}>
+        <h2>{props.header}</h2>
+      </header>
+      <form
+        onSubmit={
+          props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
+        }
+      >
+        <div className={`modal__content ${props.contentClass}`}>
+          {props.children}
+        </div>
+        <footer className={`modal__footer ${props.footerClass}`}>
+          {props.footer}
+        </footer>
+      </form>
+    </div>
   );
   return ReactDOM.createPortal(content, document.getElementById("modal-hook"));
 };
@@ -38,11 +36,7 @@ const Modal = (props) => {
         in={props.show}
         mountOnEnter
         unmountOnExit
-        timeout={{
-          appear: 200,
-          enter: 200,
-          exit: 200,
-        }}
+        timeout={200}
         classNames="modal"
       >
         <ModamOverlay {...props} />
--- src/Pages/User/Public/AllUsers.js
+++ src/Pages/User/Public/AllUsers.js
@@ -8,14 +8,20 @@ import Modal from "../../../Components/Shared/Modal";
 
 import "./AllUsers.css";
 import ErrorModal from "../../../Components/Shared/ErrorModal";
+import Waitings from "../../../Components/Shared/Waitings";
 
 const Users = () => {
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
   const [showUserProp, setshowUserProp] = useState(false);
   const [UserHighlight, setUserHighlightp] = useState();
   const [allusers, setallusers] = useState();
+
   const auth = useContext(AuthContext);
+
   const history = useHistory();
+
+  //GETUSERS AT LOADINGPAGE
   useEffect(() => {
     const sendReq = async () => {
       try {
@@ -29,26 +35,6 @@ const Users = () => {
     };
     sendReq();
   }, [sendRequest]);
-  const handlecreateconv = async (e) => {
-    const formData = new FormData();
-    formData.append("part1", auth.userId);
-    formData.append("part2", e);
-
-    try {
-      await sendRequest(
-        `http://localhost:5000/api/conv`,
-        "POST",
-        JSON.stringify({
-          part1: auth.userId,
-          part2: e,
-        }),
-        {
-          Authorization: "Bearer " + auth.token,
-          "Content-Type": "application/json",
-        }
-      );
-    } catch (err) {}
-  };
 
   //HANDLER SHOW USER USER PROPS
   const showuserpropsHandler = (usr) => {
@@ -80,6 +66,7 @@ const Users = () => {
       history.push(`/conv/${response.idconv}`);
     } catch (err) {}
   };
+
   //GOPROFIL
   const goprofileHandler = () => {
     history.push(`/${UserHighlight.id}/profil`);
@@ -93,48 +80,56 @@ const Users = () => {
         onClearAction={clearError}
         action="Go back"
       ></ErrorModal>
-      {UserHighlight && (
-        <Modal
-          show={error ? false : showUserProp}
-          onCancel={hideuserpropsHandler}
-          header={`${UserHighlight.firstname} ${UserHighlight.name}`}
-          contentClass="place-item__content"
-          footerClass="place-item__modal-actions"
-          footer={
-            <React.Fragment>
-              <button onClick={hideuserpropsHandler}> Quit</button>
-              {auth.token && UserHighlight.id !== auth.userId && (
-                <button onClick={ConvHandler}> Conv</button>
-              )}
-              <button onClick={goprofileHandler}> See profile</button>
-            </React.Fragment>
-          }
-        >
-          <Avatar
-            image={UserHighlight.img}
-            alt={UserHighlight.name}
-            width={"200px"}
-          ></Avatar>
-          <div className="bio">
-            <p>{UserHighlight.bio}</p>
-          </div>
-        </Modal>
+      {isLoading && <Waitings />}
+      {!isLoading && (
+        <React.Fragment>
+          <Modal
+            show={showUserProp}
+            onCancel={hideuserpropsHandler}
+            header={`${UserHighlight ? UserHighlight.firstname : ""}`}
+            footer={
+              <React.Fragment>
+                <button onClick={hideuserpropsHandler}> Quit</button>
+                {auth.token &&
+                  UserHighlight &&
+                  UserHighlight.id !== auth.userId && (
+                    <button onClick={ConvHandler}> Conv</button>
+                  )}
+                <button onClick={goprofileHandler}> See profile</button>
+              </React.Fragment>
+            }
+          >
+            {
+              <React.Fragment>
+                <Avatar
+                  image={UserHighlight ? UserHighlight.img : ""}
+                  alt={UserHighlight ? UserHighlight.name : ""}
+                  width={"200px"}
+                ></Avatar>
+                <div className="bio">
+                  <p>{UserHighlight ? UserHighlight.bio : ""}</p>
+                </div>
+              </React.Fragment>
+            }
+          </Modal>
+          {allusers &&
+            allusers.map((usr) => {
+              return (
+                <div key={usr.id}>
+                  <div className="name">
+                    <p>{`${usr.firstname} ${usr.name}`}</p>
+                  </div>
+                  <Avatar
+                    image={usr.img}
+                    alt={usr.id}
+                    width="100px"
+                    onClick={() => showuserpropsHandler(usr)}
+                  ></Avatar>
+                </div>
+              );
+            })}
+        </React.Fragment>
       )}
-      {allusers &&
-        allusers.map((usr) => {
-          return (
-            <div key={usr.id}>
-              <div className="name">{usr.id}</div>
-              <Avatar
-                image={usr.img}
-                alt={usr.id}
-                width="100px"
-                onClick={() => showuserpropsHandler(usr)}
-              ></Avatar>
-            </div>
-          );
-        })}
-      <div>Pas de ll</div>
     </React.Fragment>
   );
 };
